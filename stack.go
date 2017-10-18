8ee7662c6f23593af3019615fdabab543c278040
Titus Tienaah <titus.intdxdt@gmail.com>
2017-10-17 22:10:12 -0300

    stack

---
+++ stack.go
@@ -0,0 +1,64 @@
+package stack
+
+import (
+	"github.com/intdxdt/list"
+)
+
+//Stack data structure - LIFO
+type Stack struct {
+	stack *list.List
+}
+
+//NewStack - creates a new stack
+func NewStack() *Stack {
+	return &Stack{stack: list.NewList()}
+}
+
+//Size of the stack
+func (o *Stack) Size() int {
+	return o.stack.Len()
+}
+
+//Push item to the stack
+func (o *Stack) Push(item ...interface{}) *Stack {
+	for _, itm := range item {
+		o.stack.Append(itm)
+	}
+	return o
+}
+
+//Pop item from the stack
+func (o *Stack) Pop() interface{} {
+	return o.stack.Pop()
+}
+
+//First item infront
+func (o *Stack) First() interface{} {
+	return o.stack.Last()
+}
+
+//Top at the item on stack
+func (o *Stack) Top() interface{} {
+	return o.First()
+}
+
+//Last item at the end of the stack
+func (o *Stack) Last() interface{} {
+	return o.stack.First()
+}
+
+//Empty the stack
+func (o *Stack) Empty() *Stack {
+	o.stack.Clear()
+	return o
+}
+
+//IsEmpty checks if stack is empty.
+func (o *Stack) IsEmpty() bool {
+	return o.stack.Len() == 0
+}
+
+//Stringer interface
+func (o *Stack) String() string {
+	return o.stack.String()
+}
---
+++ stack_test.go
@@ -0,0 +1,45 @@
+package stack
+
+import (
+	"fmt"
+	"testing"
+	"github.com/franela/goblin"
+)
+
+func TestStack(t *testing.T) {
+	g := goblin.Goblin(t)
+
+	g.Describe("Stack", func() {
+		var array = []int{9, 5, 3, 2, 8, 1, 2, 3}
+		var obj = NewStack().Push(array[0], array[1])
+
+		for _, v := range array[2:] {
+			obj.Push(v)
+		}
+		//print
+		fmt.Println(obj)
+
+		g.It("should test items in the queue", func() {
+			g.Assert(obj.IsEmpty()).IsFalse()
+			g.Assert(obj.Size()).Equal(len(array))
+			g.Assert(obj.First().(int)).Equal(3)
+			g.Assert(obj.Top().(int)).Equal(3)
+			g.Assert(obj.Pop().(int)).Equal(3)
+			g.Assert(obj.Pop().(int)).Equal(2)
+			g.Assert(obj.Pop().(int)).Equal(1)
+			g.Assert(obj.Pop().(int)).Equal(8)
+
+			g.Assert(obj.First().(int)).Equal(2)
+			g.Assert(obj.Top().(int)).Equal(2)
+			g.Assert(obj.Last().(int)).Equal(9)
+
+			fmt.Println(obj)
+
+			obj.Empty()
+			g.Assert(obj.Pop()).Equal(nil)
+			g.Assert(obj.IsEmpty()).IsTrue()
+			//print
+			fmt.Println(obj)
+		})
+	})
+}
